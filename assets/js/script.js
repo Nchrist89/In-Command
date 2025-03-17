8d30cdf59ca37acbf49781fee509bc105a95feb7
Nick Christie <nchrist89@googlemail.com>
2025-03-17 03:48:41 +0000

    New function: Add fetch API and get question function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,46 @@ closeModal.addEventListener('click', () => {
     modal.close();
 });
 
-let questionNo = 0;
 let data = {};
+let questionNum = 0;
+let questionBox = document.getElementById("question-box");
+
+easydiff.addEventListener('click', fetchQuestions());
+
+//API URLS
+const easyQuiz = "https://opentdb.com/api.php?amount=15&category=18&difficulty=easy&type=multiple";
+let apiAddress;
+
+//Fetch API Function
+async function fetchQuestions () {
+    const response = await fetch(apiAddress);
+    data = await response.json();
+    getQuestion(data);
+}
+
+// retrieve question function
+function getQuestion () {
+    let results = data.results[questionNum];
+
+    questionBox.innerHTML = results.question;
+
+}
+
+//difficulty
+const easydiff = document.getElementById("easy-diff");
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
